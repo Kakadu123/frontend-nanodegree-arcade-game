0cc7f3b0be54defccb76ebfe5710935f4d22dca9
Sam <sg506w@SKCBC2054.emea.att.com>
2015-03-22 16:41:21 +0100

    almost done

--- js/app.js
+++ js/app.js
@@ -1,23 +1,24 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+    // Initial x coordinate
+    this.x = -101;
+    // Initial y coordinate determined by one of 3 possible rows
+    // Row assigment based on random generation
+    this.y = -27 + Math.floor((Math.random() * 3) + 1) * 83;
+    // Pace determined at object initiation in the range 80 to 240
+    this.pace = Math.random() * 160 + 80;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.x = -15;
-    this.y = -25 + Math.floor((Math.random() * 3) + 1) * 85;
-    this.pace = 2 + Math.floor(Math.random() * 3) ;
     this.sprite = 'images/enemy-bug.png';
 }
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+    // Movement multiplied by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x + this.pace;
+    this.x = this.x + this.pace * dt;
 }
 
 // Draw the enemy on the screen, required method for game
@@ -33,9 +34,7 @@ var Player = function() {
 }
 
 Player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // collision detection relating to this update method is handled in engine.js update method
 }
 
 // Draw the enemy on the screen, required method for game
@@ -46,46 +45,39 @@ Player.prototype.render = function() {
 Player.prototype.handleInput = function(clicked_button) {
     switch(clicked_button) {
         case 'up':
-        if (this.y - 85 >= 0) {
-           this.y = this.y - 85;
+        if (this.y - 83 >= 0) {
+           this.y = this.y - 83;
        }
        break;
        case 'down':
-       if (this.y + 85 <= 390) {
-           this.y = this.y + 85;
+       if (this.y + 83 <= 390) {
+           this.y = this.y + 83;
        }
        break;
        case 'left':
-       if (this.x - 100 >= 0) {
-           this.x = this.x - 100;
+       if (this.x - 101 >= 0) {
+           this.x = this.x - 101;
        }
        break;
        case 'right':
-       if (this.x + 100 <= 500) {
-           this.x = this.x + 100;
+       if (this.x + 101 <= 404) {
+           this.x = this.x + 101;
        }
        break;
    }
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
+// Objects instantiation
+// all enemy objects in an array called allEnemies
 var allEnemies = [];
 var enemy_instance = new Enemy();
 allEnemies.push(enemy_instance);
 
-// Function triggered every 400ms to maintain enemies
+// Function triggered every 1s to maintain enemies
 setInterval(
     function() {
     // Random addition of new enemies + restriction to 5 enemies max
-    if( Math.random() > 0.5 && allEnemies.length < 5)  {
+   	if( Math.random() > 0.5 && allEnemies.length < 5)  {
         var enemy_instance = new Enemy();
         allEnemies.push(enemy_instance);
     }
@@ -94,10 +86,10 @@ setInterval(
         if(allEnemies[i].x >= 505) {
            allEnemies.splice(i, 1);
        }
-    console.log(allEnemies.length);
    }
-}, 400);
+}, 800);
 
+// The player object in a variable called player
 var player = new Player();
 
 // This listens for key presses and sends the keys to your
